43a35e05dcc3ab75c1f1178a0aadb5d73f4e1d92
Rasmus Smidt <rbsmidt@gmail.com>
2015-05-08 17:12:41 +0200

    add test items

--- index.php
+++ index.php
@@ -5,10 +5,16 @@ include('header.php');
 <section>
   <div class="owl-carousel">
     <div class="item">
-      ITEM!
+      <div class="citat">
+        <h1>ITEM!</h1>
+      </div>
     </div>
     <div class="item">
-      ITEM!2
+      <div class="item">
+        <div class="citat">
+          <h1>ITEM!2</h1>
+        </div>
+      </div>
     </div>
   </div>
 </section>
