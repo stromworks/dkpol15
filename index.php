ec8358db26afe46b85d1eae7bb2d44f562515a68
Erik Hermansen <erik@stromworks.com>
2015-05-08 17:22:26 +0200

    Added loop

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
-<?php
-include('header.php');
-?>
+<?php include('header.php'); ?>
 
 <section>
   <div class="owl-carousel">
-    <div class="item">
-      
+    <?php foreach($pol_arr as $pol) : ?>
+     <div class="item" style="background-image: url('<?php echo $pol_arr['img')]; ?>'">
+      <h1><?php echo $pol_arr['citat')]; ?></h1>
+      <div class="social">
+      	<p>
+      		<img src="/assets/img/twitter.png">
+      		<span>
+      			<?php echo $pol_arr['twitter']); ?>
+      		</span>	
+      	</p>
+      </div>
     </div>
+    <?php endforeach; ?>
   </div>
 </section>
 
-<?php
-include('footer.php');
-?>
+<?php include('footer.php'); ?>
