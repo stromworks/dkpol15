f91ee7ad97794a3147f51ec2862c007c9aee167f
Rasmus Smidt <rbsmidt@gmail.com>
2015-05-08 17:07:54 +0200

    add owl.carousel.css and jquery cdn

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <!-- jQuery -->
-<script src="assets/js/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
 <script src="assets/js/owl.carousel.min.js"></script>
 <script src="assets/js/custom.js"></script>
