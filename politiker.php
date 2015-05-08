94a673171232e65a5931f2b9f7ac9677007108ae
Rasmus Smidt <rbsmidt@gmail.com>
2015-05-08 17:34:59 +0200

    corrected src error in pol arr

--- politiker.php
+++ politiker.php
@@ -2,31 +2,31 @@
 
 $pol_arr = [
     0 => [
-      'img' => '/assets/img/Lars_L_kke_Rasmusse_503613a.jpg',
+      'img' => 'assets/img/Lars_L_kke_Rasmusse_503613a.jpg',
       'citat' => 'FADØL!',
       'navn' => 'Lars Løkke Rasmussen',
       'twitter' => 'larsloekke'
     ],
     1 => [
-      'img' => '/assets/img/4715721-til-bt-joachim-b-olsen-zlem-cekic.jpg',
+      'img' => 'assets/img/4715721-til-bt-joachim-b-olsen-zlem-cekic.jpg',
       'citat' => 'Mindre alt!',
       'navn' => 'Joachim B. Olsen',
       'twitter' => 'jbolsen'
     ],
     2 => [
-      'img' => '/assets/img/7568393-sren-pape.jpg',
+      'img' => 'assets/img/7568393-sren-pape.jpg',
       'citat' => 'Flere mænd!',
       'navn' => 'Søren Pape',
       'twitter' => 'papetrolden'
     ],
     3 => [
-      'img' => '/assets/img/HTS_II_dwnload.jpg',
+      'img' => 'assets/img/HTS_II_dwnload.jpg',
       'citat' => 'Magt!',
       'navn' => 'Helle Thorning Schmidt',
       'twitter' => 'HTS'
     ],
     4 => [
-      'img' => '/assets/img/johanne_schmidt-nielsen_mark_knudsen_2015_mg_7697.jpg',
+      'img' => 'assets/img/johanne_schmidt-nielsen_mark_knudsen_2015_mg_7697.jpg',
       'citat' => 'Revolution!',
       'navn' => 'Johanne Schmidt Nielsen',
       'twitter' => 'johannesm'
