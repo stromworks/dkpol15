4f9591f3fadb7d046457ee2002317148b3252220
Erik Hermansen <erik@stromworks.com>
2015-05-11 17:58:18 +0200

    New random picture
    Added images and random quote+name

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,11 +3,37 @@ $(document).ready(function(){
 });
 
 function owlInit() {
-  var owl = $('.owl-carousel');
-  owl.owlCarousel({
+  var owlTop = $('.owl-carousel-top');
+  owlTop.owlCarousel({
     loop: true,
     margin: 0,
     nav: false,
-    items: 1
+    items: 1,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		lazyload: true
+  });
+	
+	var owlMiddle = $('.owl-carousel-middle');
+  owlMiddle.owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    items: 1,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		rtl: true,
+		lazyload: true
+  });
+	
+	var owlBottom = $('.owl-carousel-bottom');
+  owlBottom.owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    items: 1,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		lazyload: true
   });
 }
--- index.php
+++ index.php
@@ -1,26 +1,59 @@
 <?php include('header.php'); ?>
+<?php require_once('pol.php'); ?>
 
 <section>
-  <div class="owl-carousel">
-    <?php
-      foreach($pol_arr as $pol) :
-    ?>
-     <div class="item" style="background-image: url('<?php echo $pol['img']; ?>')">
-       <h2><?php echo $pol['navn']; ?></h2>
-      <h1><?php echo $pol['citat']; ?></h1>
-      <div class="social">
-      	<p>
-      		<img src="/assets/img/twitter.svg" height="30">
-      		<span>
-      			<?php echo $pol['twitter']; ?>
-      		</span>
-      	</p>
-      </div>
-    </div>
-    <?php
-      endforeach;
-    ?>
-  </div>
+ 	<div class="inner">
+ 		<div class="owl-carousel-top owl-carousel">
+			<?php
+				shuffle($pol_arr);
+				foreach($pol_arr as $pol) :
+					$name = strtolower($pol);
+					$polImg = str_replace($replace, $with, $name);
+			?>
+				<div class="item">
+					<img src="assets/img/pol/<?php echo $polImg.'_01.jpg'; ?>" />
+				</div>
+			<?php
+				endforeach;
+			?>
+		</div>
+		<div class="owl-carousel-middle owl-carousel">
+			<?php
+				shuffle($pol_arr);
+				foreach($pol_arr as $pol) :
+					$name = strtolower($pol);
+					$polImg = str_replace($replace, $with, $name);
+			?>
+				<div class="item">
+					<img src="assets/img/pol/<?php echo $polImg.'_02.jpg'; ?>" />
+				</div>
+			<?php
+				endforeach;
+			?>
+		</div>
+		<div class="owl-carousel-bottom owl-carousel">
+			<?php
+				shuffle($pol_arr);
+				foreach($pol_arr as $pol) :
+					$name = strtolower($pol);
+					$polImg = str_replace($replace, $with, $name);
+			?>
+				<div class="item">
+					<img src="assets/img/pol/<?php echo $polImg.'_03.jpg'; ?>" />
+				</div>
+			<?php
+				endforeach;
+			?>
+		</div>
+		<h2><?php echo $pol_first[rand(0,count($pol_first)-1)] . ' ' . $pol_last[rand(0,count($pol_first)-1)] . ' ' . $pol_last[rand(0,count($pol_first)-1)]; ?></h2>
+ 		<h1><span><?php echo $pol_citat[rand(0,count($pol_citat)-1)]; ?></span></h1>
+ 	</div>
 </section>
+
+<aside>
+	<div class="inner">
+		<h1>#dkpol</h1>
+	</div>
+</aside>
 
 <?php include('footer.php'); ?>
