36ad5dab092e5a2d74ce8129f5e0072a2bf3cf56
Erik Hermansen <erikshermansen@gmail.com>
2015-05-12 06:52:10 +0200

    Deleted carousel, added focus on one random pol (incl name)

--- index.php
+++ index.php
@@ -1,59 +1,45 @@
 <?php include('header.php'); ?>
 <?php require_once('pol.php'); ?>
-
+<?php
+  if( isset($_GET['pol']) ) :
+    $pol = explode('-',$_GET['pol']);
+    var_dump($pol);
+  else :
+    $pol[0] = rand(0,count($pol_arr)-1);
+    $pol[1] = rand(0,count($pol_arr)-1);
+    $pol[2] = rand(0,count($pol_arr)-1);
+  endif;
+  if( empty($pol[0]) || $pol[0] == '' ) {
+    $pol[0] = rand(0,count($pol_arr)-1);
+  }
+  if( empty($pol[1]) || $pol[1] == '' ) {
+    $pol[1] = rand(0,count($pol_arr)-1);
+  }
+  if( empty($pol[2]) || $pol[2] == '' ) {
+    $pol[2] = rand(0,count($pol_arr)-1);
+  }
+?>
 <section>
- 	<div class="inner">
- 		<div class="owl-carousel-top owl-carousel">
-			<?php
-				shuffle($pol_arr);
-				foreach($pol_arr as $pol) :
-					$name = strtolower($pol);
-					$polImg = str_replace($replace, $with, $name);
-			?>
-				<div class="item">
-					<img src="assets/img/pol/<?php echo $polImg.'_01.jpg'; ?>" />
-				</div>
-			<?php
-				endforeach;
-			?>
-		</div>
-		<div class="owl-carousel-middle owl-carousel">
-			<?php
-				shuffle($pol_arr);
-				foreach($pol_arr as $pol) :
-					$name = strtolower($pol);
-					$polImg = str_replace($replace, $with, $name);
-			?>
-				<div class="item">
-					<img src="assets/img/pol/<?php echo $polImg.'_02.jpg'; ?>" />
-				</div>
-			<?php
-				endforeach;
-			?>
-		</div>
-		<div class="owl-carousel-bottom owl-carousel">
-			<?php
-				shuffle($pol_arr);
-				foreach($pol_arr as $pol) :
-					$name = strtolower($pol);
-					$polImg = str_replace($replace, $with, $name);
-			?>
-				<div class="item">
-					<img src="assets/img/pol/<?php echo $polImg.'_03.jpg'; ?>" />
-				</div>
-			<?php
-				endforeach;
-			?>
-		</div>
-		<h2><?php echo $pol_first[rand(0,count($pol_first)-1)] . ' ' . $pol_last[rand(0,count($pol_first)-1)] . ' ' . $pol_last[rand(0,count($pol_first)-1)]; ?></h2>
- 		<h1><span><?php echo $pol_citat[rand(0,count($pol_citat)-1)]; ?></span></h1>
- 	</div>
+  <aside>
+    <div class="inner">
+      <div class="group">
+        <div class="item">
+          <img src="assets/img/pol/<?php echo str_replace($replace, $with, strtolower($pol_arr[$pol[0]]['first'] . $pol_arr[$pol[0]]['last'])) . '_01.jpg'; ?>" />
+        </div>
+        <div class="item">
+          <img src="assets/img/pol/<?php echo str_replace($replace, $with, strtolower($pol_arr[$pol[1]]['first'] . $pol_arr[$pol[1]]['last'])) . '_02.jpg'; ?>" />
+        </div>
+        <div class="item">
+          <img src="assets/img/pol/<?php echo str_replace($replace, $with, strtolower($pol_arr[$pol[2]]['first'] . $pol_arr[$pol[2]]['last'])) . '_03.jpg'; ?>" />
+        </div>
+      </div>
+    </div>
+  </aside>
+  <article>
+    <div class="inner">
+      <h1><?php echo $pol_arr[$pol[0]]['first'] . ' ' . $pol_arr[$pol[1]]['last'] . ' ' . $pol_arr[$pol[2]]['last']; ?></h1>
+    </div>
+  </article>
 </section>
-
-<aside>
-	<div class="inner">
-		<h1>#dkpol</h1>
-	</div>
-</aside>
 
 <?php include('footer.php'); ?>
--- pol.php
+++ pol.php
@@ -1,119 +1,139 @@
 <?php
 
-$pol_arr = array(
-	'Lars Løkke Rasmussen', 
-	'Helle Thorning Schmidt',
-	'Kristian Thuelsen Dahl',
-	'Anders Samuelsen',
-	'Bertel Haarder',
-	'Bjarne Corydon',
-	'Brian Mikkelsen',
-	'Christine Antorini',
-	'Claus Hjort Frederiksen',
-	'Dan Jørgensen',
-	'Frank Aaen',
-	'Holger K. Nielsen',
-	'Ida Auken',
-	'Inger Støjberg',
-	'Joachim B. Olsen',
-	'Karen Ellemann',
-	'Johanne Schmidt Nielsen',
-	'Kristian Jensen',
-	'Magnus Heunicke',
-	'Marianne Jelved',
-	'Martin Henriksen',
-	'Mette Frederiksen',
-	'Mogens Lykketoft',
-	'Søren Gade',
-	'Zenia Stampe',
-	'Morten Østergaard',
-	'Naser Khader',
-	'Pernille Skipper',
-	'Pia Kjærsgaard',
-	'Simon Emil Ammitzbøll',
-	'Søren Pind',
-	'Søren Espersen',
-	'Uffe Elbæk'
-);
-
-$pol_first = array(
-	'Lars', 
-	'Helle',
-	'Kristian',
-	'Anders',
-	'Bertel',
-	'Bjarne',
-	'Brian',
-	'Christine',
-	'Claus',
-	'Dan',
-	'Frank',
-	'Holger',
-	'Ida',
-	'Inger',
-	'Joachim',
-	'Karen',
-	'Johanne',
-	'Kristian',
-	'Magnus',
-	'Marianne',
-	'Martin',
-	'Mette',
-	'Mogens',
-	'Søren',
-	'Zenia',
-	'Morten',
-	'Naser',
-	'Pernille',
-	'Pia',
-	'Simon Emil',
-	'Søren',
-	'Søren',
-	'Uffe'
-);
-
-$pol_last = array(
-	'Løkke',
-	'Rasmussen', 
-	'Thorning',
-	'Schmidt',
-	'Thuelsen',
-	'Dahl',
-	'Samuelsen',
-	'Haarder',
-	'Corydon',
-	'Mikkelsen',
-	'Antorini',
-	'Hjort',
-	'Frederiksen',
-	'Jørgensen',
-	'Aaen',
-	'K.',
-	'Nielsen',
-	'Auken',
-	'Støjberg',
-	'B.',
-	'Olsen',
-	'Ellemann',
-	'Schmidt',
-	'Nielsen',
-	'Jensen',
-	'Heunicke',
-	'Jelved',
-	'Henriksen',
-	'Frederiksen',
-	'Lykketoft',
-	'Gade',
-	'Stampe',
-	'Østergaard',
-	'Khader',
-	'Skipper',
-	'Kjærsgaard',
-	'Ammitzbøll',
-	'Pind',
-	'Espersen',
-	'Elbæk'
-);
+$pol_arr = [
+	'0' => [
+		'first' => 'Lars',
+		'last' => 'Løkke Rasmussen'
+	],
+	'1' => [
+		'first' => 'Helle',
+		'last' => 'Thorning Schmidt'
+	],
+	'2' => [
+		'first' => 'Kristian',
+		'last' => 'Thuelsen dahl'
+	],
+	'3' => [
+		'first' => 'Anders',
+		'last' => 'Samuelsen'
+	],
+	'4' => [
+		'first' => 'Bertel',
+		'last' => 'Haarder'
+	],
+	'5' => [
+		'first' => 'Bjarne',
+		'last' => 'Corydon'
+	],
+	'6' => [
+		'first' => 'Brian',
+		'last' => 'Mikkelsen'
+	],
+	'7' => [
+		'first' => 'Christine',
+		'last' => 'Antorini'
+	],
+	'8' => [
+		'first' => 'Claus',
+		'last' => 'Hjort Frederiksen'
+	],
+	'9' => [
+		'first' => 'Dan',
+		'last' => 'Jørgensen'
+	],
+	'10' => [
+		'first' => 'Frank',
+		'last' => 'Aaen'
+	],
+	'11' => [
+		'first' => 'Holger',
+		'last' => 'K. Nielsen'
+	],
+	'12' => [
+		'first' => 'Ida',
+		'last' => 'Auken'
+	],
+	'13' => [
+		'first' => 'Inger',
+		'last' => 'Støjberg'
+	],
+	'14' => [
+		'first' => 'Joachim',
+		'last' => 'B. Olsen'
+	],
+	'15' => [
+		'first' => 'Karen',
+		'last' => 'Ellemann'
+	],
+	'16' => [
+		'first' => 'Johanne',
+		'last' => 'Schmidt-Nielsen'
+	],
+	'17' => [
+		'first' => 'Kristian',
+		'last' => 'Jensen'
+	],
+	'18' => [
+		'first' => 'Magnus',
+		'last' => 'Heunicke'
+	],
+	'19' => [
+		'first' => 'Marianne',
+		'last' => 'Jelved'
+	],
+	'20' => [
+		'first' => 'Martin',
+		'last' => 'Henriksen'
+	],
+	'21' => [
+		'first' => 'Mette',
+		'last' => 'Frederiksen'
+	],
+	'22' => [
+		'first' => 'Mogens',
+		'last' => 'Lykketoft'
+	],
+	'23' => [
+		'first' => 'Søren',
+		'last' => 'Gade'
+	],
+	'24' => [
+		'first' => 'Zenia',
+		'last' => 'Stampe'
+	],
+	'25' => [
+		'first' => 'Morten',
+		'last' => 'Østergaard'
+	],
+	'26' => [
+		'first' => 'Naser',
+		'last' => 'Khader'
+	],
+	'27' => [
+		'first' => 'Pernille',
+		'last' => 'Skipper'
+	],
+	'28' => [
+		'first' => 'Pia',
+		'last' => 'Kjærsgaard'
+	],
+	'29' => [
+		'first' => 'Simon Emil',
+		'last' => 'Ammitzbøll'
+	],
+	'30' => [
+		'first' => 'Søren',
+		'last' => 'Pind'
+	],
+	'31' => [
+		'first' => 'Uffe',
+		'last' => 'Elbæk'
+	],
+	'32' => [
+		'first' => 'Søren',
+		'last' => 'Espersen'
+	]
+];
 
 $pol_citat = array(
 	'Mere service',
